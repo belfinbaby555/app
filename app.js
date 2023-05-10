6349c0c9fabe2641e32be3e9e064dfd89cd44576
belfinbaby555 <105835749+belfinbaby555@users.noreply.github.com>
2023-05-10 09:46:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello world!');
 });
-server.listen(process.env.PORT);
+server.listen(8000);
